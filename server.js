8bb0816c938e34340eca4d57e6b6fef21db4ac52
PreethiAnnJacob <preethiannjacob1998@gmail.com>
2017-03-23 20:31:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config={
     database:'preethiannjacob',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password:process.env.DB_PASSWORD //create environment variable
 };
 
 app.use(express.static('ui'));
