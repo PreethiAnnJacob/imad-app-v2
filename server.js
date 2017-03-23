082d7c214fc83b86590ebe35e3fee8f44563fc0e
PreethiAnnJacob <preethiannjacob1998@gmail.com>
2017-03-23 18:59:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,30 +5,113 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={ 
+   			'article-one':{
+				title:"Article One | Preethi Ann Jacob",
+				heading:'Article One',
+				date:'Sep 5, 2016',
+				content:`
+				         <p>
+  							This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
+  						</p>
+  						<p>
+  							This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
+  					   </p>
+  					   <p>
+  							This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
+  					   </p>`
+			},
+			'article-two':{
+				title:'Article Two | Preethi Ann Jacob',
+				heading:'Article Two',
+				date: 'Feb 5, 2017',
+				content: `
+				    <p>
+  							This is the content of my second article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
+  					</p> `
+			},
+			'article-three':{
+				title:'Article Three | Preethi Ann Jacob',
+				heading:'Article Three',
+				date: 'Feb 15, 2017',
+				content: `
+				    <p>
+  							This is the content of my third article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
+  					</p> `
+			}
+ };
+function createTemplate(data){
+	var title=data.title;
+	var date=data.date;
+	var heading=data.heading;
+	var content=data.content;
+
+	var htmlTemplate=`
+	<html>
+  	<head>
+  	<title>
+  		${title}
+  	</title>
+  	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+  	</head>
+  	<body>
+  		<div>
+  			<a href="/">Home</a>
+  		</div>
+  		<hr/>
+  		<h3>
+  			${heading}
+  		</h3>
+  		<div> 
+  			${date}
+  		</div>
+  		<div>
+  			${content}
+  		</div>
+  	</body>
+	</html>
+  `; 
+  return htmlTemplate;
+
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+var counter=0;
+app.get('/counter',function(req,res){
+  counter=counter+1;
+  res.send(counter.toString());
 });
 
-app.get('/article-two',function(req,res){
-    res.send("Article-two requested and will be served here");
+var names=[];
+app.get('/submit-name',function(req,res){
+    //Get the name from the request
+    var name=req.query.name;
+    
+    names.push(name);
+    //JSON: Javascript Object Notation
+    res.send(JSON.stringify(names));
 });
 
-app.get('/article-three',function(req,res){
-    res.send("Article-three requested and will be served here");
+app.get('/:articleName', function(req,res){
+	//articleName==article-one
+	//article(articleName={} content object for article-one
+	var articleName=req.params.articleName;
+	res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
