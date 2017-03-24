6685588370ab540677f5a2c61afb290008a681d3
PreethiAnnJacob <preethiannjacob1998@gmail.com>
2017-03-24 15:07:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -39,41 +39,7 @@ app.get('/test-db',function(req,res){
     });
 });   
 
-var articles={ 
-   			'article-one':{
-				title:"Article One | Preethi Ann Jacob",
-				heading:'Article One',
-				date:'Sep 5, 2016',
-				content:`
-				         <p>
-  							This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
-  						</p>
-  						<p>
-  							This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
-  					   </p>
-  					   <p>
-  							This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
-  					   </p>`
-			},
-			'article-two':{
-				title:'Article Two | Preethi Ann Jacob',
-				heading:'Article Two',
-				date: 'Feb 5, 2017',
-				content: `
-				    <p>
-  							This is the content of my second article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
-  					</p> `
-			},
-			'article-three':{
-				title:'Article Three | Preethi Ann Jacob',
-				heading:'Article Three',
-				date: 'Feb 15, 2017',
-				content: `
-				    <p>
-  							This is the content of my third article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
-  					</p> `
-			}
- };
+
 function createTemplate(data){
 	var title=data.title;
 	var date=data.date;
